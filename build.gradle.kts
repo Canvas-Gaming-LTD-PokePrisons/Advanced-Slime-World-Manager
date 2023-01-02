88cdb45118c86ef32a74dda30f48e72084f5137c
Hasan Demirtaş <utsukushihito@outlook.com>
2023-01-02 20:07:35 +0300

    with source and javadoc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,11 +51,18 @@ allprojects {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(17))
         }
+        withSourcesJar()
+        withJavadocJar()
     }
 }
 
 subprojects {
     apply<MavenPublishPlugin>()
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
+    }
 
     publishing {
         publications {
@@ -67,41 +74,7 @@ subprojects {
                 from(components["java"])
 
                 pom {
-                    name.set("Advanced Slime World Manager API")
-                    description.set("API for ASWM")
-                    url.set("https://github.com/Paul19988/Advanced-Slime-World-Manager")
-                    licenses {
-                        license {
-                            name.set("GNU General Public License, Version 3.0")
-                            url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("InfernalSuite")
-                            name.set("The InfernalSuite Team")
-                            url.set("https://github.com/InfernalSuite")
-                            email.set("infernalsuite@gmail.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:https://github.com/Paul19988/Advanced-Slime-World-Manager.git")
-                        developerConnection.set("scm:git:ssh://github.com/Paul19988/Advanced-Slime-World-Manager.git")
-                        url.set("https://github.com/Paul19988/Advanced-Slime-World-Manager/")
-                    }
-                    issueManagement {
-                        system.set("Github")
-                        url.set("https://github.com/Paul19988/Advanced-Slime-World-Manager/issues")
-                    }
-                }
-
-                versionMapping {
-                    usage("java-api") {
-                        fromResolutionOf("runtimeClasspath")
-                    }
-                    usage("java-runtime") {
-                        fromResolutionResult()
-                    }
+                    name.set(project.name)
                 }
             }
         }
--- slimeworldmanager-api/build.gradle.kts
+++ slimeworldmanager-api/build.gradle.kts
@@ -6,10 +6,3 @@ dependencies {
     implementation("com.flowpowered:flow-nbt:2.0.2")
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
-description = "slimeworldmanager-api"
